d0a812899cd978690a86b0479e5d12163c2c43c1
Nathchill <camiloquiroga234@gmail.com>
2024-05-07 07:26:53 -0500

    hito1

--- index.js
+++ index.js
@@ -16,6 +16,44 @@ const POKEMON_TYPES_ICONS = {
 }
 
 console.log(data)
+
 // Tu código aquí
 
-console.log("Prueba no más, sisa una prueba")
+const pokemonData = data.pokemon;
+
+
+function renderPokemon(pokemon) {
+  const typeLabel = pokemon.type[0]; 
+  const typeLabelClass = typeLabel.toLowerCase(); 
+
+  const markup = `
+    <div class="pokemon-card ${typeLabelClass}">
+      <div class="pokemon-card__type-label ${typeLabelClass}">
+        <i class="pokemon-card__name-icon bx bx-${typeLabel.toLowerCase()} ${typeLabelClass}"></i>
+        <span>${typeLabel.toLowerCase()}</span>
+      </div>
+      <div class="pokemon-card__image">
+        <img src="${pokemon.img}" alt="${pokemon.name}" loading="lazy">
+      </div>
+      <div class="pokemon-card__name">
+        <h4>${pokemon.name}</h4>
+      </div>
+      <p class="pokemon-card__eyebrow">#${pokemon.num}</p>
+    </div>
+  `;
+
+  return markup;
+}
+
+
+const cardsContainer = document.querySelector('.cards');
+
+
+let pokemonCards = '';
+pokemonData.forEach(pokemon => {
+  pokemonCards += renderPokemon(pokemon);
+});
+
+
+cardsContainer.innerHTML = pokemonCards;
+
